c50a63ea50bc2e6ea2b9fa9b50255a7d5a4f446c
Daniel Katz <daniekatz@gmail.com>
2018-08-06 23:15:30 +0300

    Initial commit in Honeycomb project

---
+++ src/Honeycomb/DependencyObject.cs
@@ -0,0 +1,18 @@
+﻿using Honeycomb.Xaml;
+using Honeycomb.Xaml.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Honeycomb
+{
+    public abstract class DependencyObject : IDependencyObject
+    {
+        public DependencyObject()
+        {
+            Component = Dependency.Component.Create(this);
+        }
+
+        public IDependencyComponent Component { get; }
+    }
+}
